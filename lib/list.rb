48d7baf0112894153dc747a3d81fad4306a5e3a7
Leila Weiss <leila.weiss@powerhrg.com>
2019-04-11 16:25:13 -0400

    update

---
+++ lib/list.rb
@@ -0,0 +1,19 @@
+class List
+  attr_accessor :rank, :title, :overview, :url, :table
+  @@all = []
+
+  def initialize(rank:, title:, overview:, url:, table: nil)
+    @rank = rank
+    @title = title
+    @overview = overview
+    @url = url
+    @table = table
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 # handles scraping and creating book and category objects
 class Scraper
+  attr_accessor :list
 
 
   def get_page
@@ -13,10 +14,17 @@ class Scraper
    title = option.css('.col h4').text.strip[4..-1]
    overview = option.css('p').text.strip
    url = "https://thegreatestbooks.org" + option.css('.col h4 a').first.attr("href")
-   binding.pry
+   @list = List.new(rank: rank, title: title, overview: overview, url: url)
   end
  end
 
+  def showing_list(list)
+    show_list = Nokogiri::HTML(open(list.url))
+    show_list.css(".row pt-3").each do |listing|
+      list.table = listing.css(".list-unstyled").text
+   binding.pry
+    end
+  end
 
 end
 

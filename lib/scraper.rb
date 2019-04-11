68547dd7ec2fe162adaf5200cd37790fa23d39c6
Leila Weiss <leila.weiss@powerhrg.com>
2019-04-11 13:00:00 -0400

    new website

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,29 +1,25 @@
 # handles scraping and creating book and category objects
 class Scraper
-  attr_accessor :categories, :books, :doc
+
 
   def get_page
-  Nokogiri::HTML(open("https://www.barnesandnoble.com/b/top-books-of-the-month/_/N-2luc"))
+    Nokogiri::HTML(open("https://thegreatestbooks.org/"))
   end
 
-  def scrape_categories
-    categories_elements = categories_elements.css(".rule").each do |element|
-      categories_elements
-      #title = element.css(".product-shelf-title")
-      binding.pry
-    end
-  end
-
-  # def scrape_books
-   #   book_elements = doc.css("hotBooksWithDesc_#{scrape_categories}")
-   #   #binding.pry
-   # end
-
-  # end
-
-
+  def get_list
+   page = get_page
+   options = page.css('.item.pb-3.pt-3.border-bottom')
+   rank_list = options.css('h4')
+   overview = options.css('p')
+   binding.pry
+ end
 end
 
+
+
+# page.css('.item.pb-3.pt-3.border-bottom')[0].css(p)
+# page.css('.item.pb-3.pt-3.border-bottom')[0].css('h4')
+# page.css('.item.pb-3.pt-3.border-bottom')
 
 
 
